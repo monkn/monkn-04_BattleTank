dc66db01612f8a4eefa55e9c75c6d8d6f23e8d79
monkn <monkn@users.noreply.github.com>
2016-10-23 11:11:26 -0400

    Section 4, Lecture 115 - Using virtual and override

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+
+#include "TankPlayerController.h"
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto Tank = GetControlledTank();
+	if (Tank) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Controlled Tank: %s"), *(Tank->GetName()));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Controlled tank is NULL"));
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
